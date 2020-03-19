999b2f700b9471c3819567a264f17740d5ad0093
Nathan Malnoury <n.malnoury@gmail.com>
2020-03-19 18:01:41 +0000

    WIP

--- hotels/models/hotel.py
+++ hotels/models/hotel.py
@@ -1,12 +1,25 @@
+"""Implement class Hotel."""
 import os
 
 from hotels.utils.conf import Conf
 
 
 class Hotel:
+    """Store hotel information."""
+
     root_url = os.path.dirname(Conf()["TRIP_ADVISOR"]["base_url"])
 
     def __init__(self, name, rating, price, currency, detail_url, needs_root=True):
+        """
+        Initialise Hotel object.
+
+        :param name: Name of Hotel
+        :param rating: Rating of Hotel
+        :param price: Price of one night at the Hotel
+        :param currency: Currency in which the price is given
+        :param detail_url: Url to the detailed page of the Hotel
+        :param needs_root: If the url needs the start of the url 'https://tripadvisor.co.uk' or not
+        """
         self.is_complete = all([name, rating, price, detail_url])
         self.name = name
         self.rating = rating
@@ -16,6 +29,14 @@ class Hotel:
 
     @staticmethod
     def from_json(list_of_dict):
+        """
+        Transform a list of dict from a json into a list off Hotel.
+
+        :param list_of_dict: list of hotels read from json
+        :type list_of_dict: list[dict]
+        :return: list of Hotels
+        :rtype: list[Hotel]
+        """
         list_hotels = []
         for hotel in list_of_dict:
             list_hotels.append(
